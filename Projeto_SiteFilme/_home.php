701b798bbdeb752bd238c788ab23af818ce9fb81
jrgabriel28 <gabrieldeiro2018@gmail.com>
2023-06-02 22:02:11 -0300

    Update _home.php

--- Projeto_SiteFilme/_home.php
+++ Projeto_SiteFilme/_home.php
@@ -8,7 +8,7 @@
         echo "
             <div class='col-sm-3 text-center p-3'>
                 <a href='#'>
-                <div class='card'>
+                <div class='card text-center'>
                     <img src = img/$row[3] class='cfilme'>
                     <h3>$row[2]</h3>
                     <h3 class='nota'>Nota:$row[5]</h3>
