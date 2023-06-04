03b5f97cf460ea6ac085adae41f7008702e5b9c4
josephfranca <116111572+josephfranca@users.noreply.github.com>
2023-06-03 21:13:05 -0300

    Add files via upload
    espero que esteja certo

---
+++ detalhe_filme.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <?php
+            include_once('testeconexao.php');
+
+            $sql = $conn->query('select * from filme where id_filme =' . ($_GET["id"]));
+
+            foreach ($sql as $row) {
+                echo "
+                    <div class = 'col-sm-4 text-center p-3>
+                    <a href= '#'>
+                    <div class = 'card'>
+                        <img src=''$row[3]
+                        <h3> $row[2]</h3>
+                    </div>
+                    .<div class=  'col-sm-8 text-justify p-3>
+                        <p> $row[4] </p>
+                    </div>
+
+                        <hr>
+
+                    <div class = 'row'> 
+                    
+                        <div class = 'col-sm-4 text-center p-3>
+                            <h3> Nota: $row[5] </h3>       
+                            </div>
+                    </div>
+
+                    <div class = 'col-sm-4 text-center p-3'>
+                        <h3>Nome: $row[1]</h3>
+                    </div>
+
+                    <div class = 'col-sm-4 text-center p-3'>
+                        <h3>Sinopse: $row[3]</h3>
+                    </div>
+                "; //lembre de trocar os valores das rows de acordo com o banco de dados.
+            }
+
+            ?>
+        </div>
+    </div>
+
+</body>
+
+</html>
